56a183d6688e8021d798426c9572cbd70bcc586d
Yushan Lin <yushan@uber.com>
2026-04-30 14:30:21 -0700

    update

--- core/targethasher/graph_test.go
+++ core/targethasher/graph_test.go
@@ -22,7 +22,7 @@ import (
 
 	buildpb "github.com/bazelbuild/buildtools/build_proto"
 	set "github.com/deckarep/golang-set/v2"
-	"go.uber.org/mock/gomock"
+	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-cmp/cmp/cmpopts"
 	"github.com/stretchr/testify/assert"
