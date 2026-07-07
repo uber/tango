47c2bd179fdca1c535b3bf20f324f952edc276ba
Sung Yoon Whang <sungyoon@uber.com>
2026-07-06 17:19:38 -0700

    git merge somehow dup'd one test, fixing

--- core/workspace/request_test.go
+++ core/workspace/request_test.go
@@ -36,20 +36,6 @@ func TestNewRequest_Github_Success(t *testing.T) {
 	require.Nil(t, gr.git)
 }
 
-func TestNewRequest_Github_Success(t *testing.T) {
-	rawURL := "github://org/repo/pull/123"
-	var g git.Interface = nil
-
-	req, err := NewRequest(rawURL, g, "baseRef", "abc123", zap.NewNop().Sugar())
-	require.NoError(t, err)
-	require.NotNil(t, req)
-	gr, ok := req.(*gitRequest)
-	require.True(t, ok, "returned Request should be *gitRequest")
-	require.Equal(t, "123", gr.requestID)
-	require.Equal(t, "abc123", gr.commit)
-	require.Nil(t, gr.git)
-}
-
 func TestNewRequest_InvalidURL(t *testing.T) {
 	rawURL := "://bad"
 	var g git.Interface = nil
@@ -61,8 +47,9 @@ func TestNewRequest_InvalidURL(t *testing.T) {
 
 func TestNewRequest_InvalidScheme(t *testing.T) {
 	rawURL := "phabricator://bad"
+	var g git.Interface = nil
 
-	_, err := NewRequest(rawURL, g, "baseRef", "", zap.NewNop().Sugar())
+	req, err := NewRequest(rawURL, g, "baseRef", "", zap.NewNop().Sugar())
 	require.Error(t, err)
 	require.Nil(t, req)
 }
